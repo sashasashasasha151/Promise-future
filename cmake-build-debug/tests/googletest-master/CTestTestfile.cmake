b18a58be9d17cfad44afdea14102d1df58128d7c
Sasha Pakulev <sashasashasasha151@gmail.com>
2018-10-20 19:27:04 +0300

    Remove directoties and files

--- cmake-build-debug/tests/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Google/CodeProjects/C++/promise_future/tests/googletest-master
-# Build directory: C:/Google/CodeProjects/C++/promise_future/cmake-build-debug/tests/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/tests/googletest-master/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Google/CodeProjects/C++/promise_future/tests/googletest-master/googletest
-# Build directory: C:/Google/CodeProjects/C++/promise_future/cmake-build-debug/tests/googletest-master/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
